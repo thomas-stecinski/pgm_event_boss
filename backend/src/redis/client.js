5450955591c9a5b1357f4f0d176941a33739f35e
MatNgl <mattheo.naegellen@gmail.com>
2026-02-10 15:22:53 +0100

    correction error and optionnal zod

--- backend/src/redis/client.js
+++ backend/src/redis/client.js
@@ -3,7 +3,7 @@ const { env } = require("../config/env");
 
 const redis= new Redis(env.REDIS_URL);
 
-redis.on("Connect", () => console.log("[redis] connected"));
-redis.on("Error", (e) => console.error("[redis] error", e))
+redis.on("connect", () => console.log("[redis] connected"));
+redis.on("error", (e) => console.error("[redis] error", e))
 
 module.exports = { redis };
